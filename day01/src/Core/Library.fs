f26cfd977d34e0b6c79c0e61eb63a141d841c422
Allan Menoret <allan.menoret@gmail.com>
2020-12-01 23:04:45 +0100

    day01 done

---
+++ day01/src/Core/Library.fs
@@ -0,0 +1,10 @@
+﻿namespace Core
+
+module Report =
+    let fixExpense (expenses: int list) =
+        let (x,y) = expenses |> List.allPairs expenses |> List.find (fun (x, y) -> x+y = 2020)
+        x*y
+
+    let fixExpense2 (expenses: int list) =
+        let (x,(y,z)) = expenses |> List.allPairs expenses |> List.allPairs expenses |> List.find (fun (x, (y, z)) -> x+y+z = 2020)
+        x*y*z
