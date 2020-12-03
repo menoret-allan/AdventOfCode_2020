a0cb7fef432d868d90f7292b5a2e1ebfaecb60e9
Allan Menoret <allan.menoret@gmail.com>
2020-12-03 16:20:53 +0100

    day03 part2

--- day03/tests/Core.Tests/Tests.fs
+++ day03/tests/Core.Tests/Tests.fs
@@ -13,3 +13,15 @@ let ``7 trees on the path`` () =
 [<Fact>]
 let ``big map`` () =
     Inputs.bigMap |> parseMap |> howManyTrees (3, 1) |> should equal 169
+
+
+[<Fact>]
+let ``small map / part 2 map`` () =
+    let map = Inputs.smallMap |> parseMap
+    [(1,1);(3,1);(5,1);(7,1);(1,2)] |> List.fold (fun x mov -> (howManyTrees mov map) * x) 1 |> should equal 336
+
+
+[<Fact>]
+let ``part 2 map`` () =
+    let map = Inputs.bigMap |> parseMap
+    [(1,1);(3,1);(5,1);(7,1);(1,2)] |> List.fold (fun x mov -> (bigint (howManyTrees mov map)) * x) 1I |> should equal 7560370818I
