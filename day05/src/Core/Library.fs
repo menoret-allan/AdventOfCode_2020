30843cfc70f5bc8324ffb40d6f673a8f6ed02ad6
Allan Menoret <allan.menoret@gmail.com>
2020-12-05 16:05:26 +0100

    day 05 part 1

---
+++ day05/src/Core/Library.fs
@@ -0,0 +1,31 @@
+﻿namespace Core
+
+module Plane =
+
+    let computeRow row =
+        let rec compute row count =
+            match row with
+            | [] -> 0
+            | 'B'::rest -> (pown 2 count) + compute rest (count - 1)
+            | 'F'::rest -> compute rest (count - 1)
+            | _ -> failwith "false letter in row"
+        compute row 6
+
+    let computeCol col =
+        let rec compute col count =
+            match col with
+            | [] -> 0
+            | 'R'::rest -> (pown 2 count) + compute rest (count - 1)
+            | 'L'::rest -> compute rest (count - 1)
+            | _ -> failwith "false letter in col"
+        compute col 2
+
+
+    let getRowCol (board:string) =
+        let row = board |> Seq.take 7 |> List.ofSeq |> computeRow
+        let col = board |> Seq.skip 7 |> List.ofSeq |> computeCol
+        (row, col)
+
+    let getId (board: string) =
+        let (row, col) = getRowCol board
+        row * 8 + col
---
+++ day05/tests/Core.Tests/Tests.fs
@@ -0,0 +1,26 @@
+module Tests
+
+open System
+open Xunit
+open FsUnit
+
+open Inputs
+open Core.Plane
+
+[<Theory>]
+[<InlineData("BFFFBBFRRR", 70, 7)>]
+[<InlineData("FFFBBBFRRR", 14, 7)>]
+[<InlineData("BBFFBBFRLL", 102, 4)>]
+let ``Compute row col`` (input, expectedRow, expectedCol) =
+    input |> getRowCol |> should equal (expectedRow, expectedCol)
+
+[<Theory>]
+[<InlineData("BFFFBBFRRR", 567)>]
+[<InlineData("FFFBBBFRRR", 119)>]
+[<InlineData("BBFFBBFRLL", 820)>]
+let ``GetId`` (input, expectedId) =
+    input |> getId |> should equal expectedId
+
+[<Fact>]
+let ``big input`` () =
+    bigSet.Split '\n' |> Array.map getId |> Array.max |> should equal 42
