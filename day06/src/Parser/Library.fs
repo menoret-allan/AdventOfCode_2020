448d055c3efe3fc767ca368fd980f5ca0133c9be
Allan Menoret <allan.menoret@gmail.com>
2020-12-06 17:25:26 +0100

    day06 part2

--- day06/src/Parser/Library.fs
+++ day06/src/Parser/Library.fs
@@ -6,3 +6,17 @@ module Form =
         oneLine.Split "  " |>
             List.ofArray |>
             List.map (fun x -> x |> Seq.filter (fun c -> c <> ' ') |> Seq.distinct |> Seq.length)
+
+
+    let merge (acc:string) (v:string) =
+        acc |> Seq.append v |>
+            Seq.groupBy id |>
+            Seq.filter (fun (_, v) -> Seq.length v = 2) |> Seq.map (fun (key, _) -> key) |>
+            Seq.toArray |>
+            System.String
+        
+    let joinForm2 (forms: string) =
+        let oneLine = forms.Split '\n' |> List.ofArray |> List.reduce (fun acc x -> acc + " " + x)
+        let result = oneLine.Split "  " |> List.ofArray |> List.map (fun x -> x.Split ' ' |> Array.reduce merge)
+        result |> List.sumBy (fun x -> x.Length)
+
--- day06/tests/Core.Tests/Tests.fs
+++ day06/tests/Core.Tests/Tests.fs
@@ -14,3 +14,21 @@ let ``My test`` () =
 [<Fact>]
 let ``My test big`` () =
     bigSet |> joinForm |>  List.sum |> should equal 6430
+
+[<Theory>]
+[<InlineData("abc", "abc", "abc")>]
+[<InlineData("abc", "", "")>]
+[<InlineData("", "abc", "")>]
+[<InlineData("aebcd", "xabyc", "abc")>]
+let ``merge`` (acc, str, expected) =
+    merge acc str |> should equal expected
+
+
+
+[<Fact>]
+let ``My test part 2`` () =
+    smallSet |> joinForm2 |> should equal 6
+
+[<Fact>]
+let ``My test big part 2`` () =
+    bigSet |> joinForm2 |> should equal 3125
