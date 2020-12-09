078fb9a1a7c8a6e98533f7af716d5ad8b0d3db42
Allan Menoret <allan.menoret@gmail.com>
2020-12-09 22:21:26 +0100

    day09 part 2

--- day09/src/Core/Library.fs
+++ day09/src/Core/Library.fs
@@ -10,9 +10,29 @@ module Xmas =
 
     let rec tillFailure pos (input: int64 array) =
         match compute pos input with
-        | Some x -> x
+        | Some x -> (pos+25,x)
         | None -> tillFailure (pos+1) input
 
     let scan (str:string) =
         let input = str.Split '\n' |> Array.map int64
-        tillFailure 0 input
+        let (_, x) = tillFailure 0 input
+        x
+
+    let rec generateList target input list =
+        match (target - (list |> List.sum), input, list) with
+        | (0L, [], _) -> seq {yield list}
+        | (0L, n::rest, _) -> seq {yield list; yield! generateList target rest (list @ [n])}
+        | (_, [], _) -> Seq.empty
+        | (x, n::rest, _) when x > 0L -> seq {yield! generateList target rest (list @ [n])}
+        | (x, _, n::rest) when x < 0L -> seq {yield! generateList target input rest}
+        | _ -> failwith "no match inside hte list generator"
+
+
+
+    let scan2 (str:string) =
+        let input = str.Split '\n' |> Array.map int64
+        let (pos, x) = tillFailure 0 input
+        let inputReduced = input |> Array.take pos |> Array.toList
+        let (_, list) = (generateList x inputReduced []) |> Seq.map (fun x -> (List.length x, x)) |> Seq.minBy (fun (s,_) -> s)
+        List.max list + (List.min list)
+
--- day09/tests/Core.Tests/Tests.fs
+++ day09/tests/Core.Tests/Tests.fs
@@ -8,8 +8,16 @@ open Core.Xmas
 
 [<Fact>]
 let ``small set test`` () =
-    smallSet |> scan |> should equal 999
+    smallSet |> scan |> should equal 204L
 
 [<Fact>]
 let ``big set test`` () =
     bigSet |> scan |> should equal 1212510616L
+
+[<Fact>]
+let ``small set test part 2`` () =
+    smallSet |> scan2 |> should equal 102L
+
+[<Fact>]
+let ``big set test part 2`` () =
+    bigSet |> scan2 |> should equal 171265123L
