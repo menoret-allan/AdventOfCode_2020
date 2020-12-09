33a3d972003f44c29ff2bf61f717ca08a030dce4
Allan Menoret <allan.menoret@gmail.com>
2020-12-09 13:14:04 +0100

    day08 part 2

--- day08/src/Core/Library.fs
+++ day08/src/Core/Library.fs
@@ -17,3 +17,30 @@ module Inst =
 
     let compute instructions =
         instructions |> iterTillLoop 1 0 []
+
+    let rec iterTillLoop3 pos acc prevPos instructions =
+        match (prevPos |> List.contains pos, (pos > Array.length instructions)) with
+        | (_, true) -> Some acc
+        | (true, _) -> None
+        | (_, _) -> match computePos pos acc instructions with| (x,newAcc) -> iterTillLoop3 x newAcc (pos::prevPos) instructions
+
+    let iterTillLoop2 opt instructions =
+        if opt <> None then opt
+        else iterTillLoop3 1 0 [] instructions
+
+    let createNewInst instructions pos newVal =
+        let t =  Array.copy instructions
+        t.[pos] <- newVal
+        t
+
+    let generatePossibilities instructions list (pos, inst) =
+        match inst with
+        | Nop n when n <> 0 -> (createNewInst instructions pos (Jmp n))::list
+        | Jmp n -> (createNewInst instructions pos (Nop n))::list
+        | _ -> list
+
+    let compute2 instructions =
+        let result = instructions |> Array.mapi (fun i x -> (i,x)) |> Array.fold (generatePossibilities instructions) [instructions] |> List.fold iterTillLoop2 None
+        match result with
+            | Some acc -> acc
+            | None -> failwith "nothing found"
--- day08/tests/Core.Tests/Tests.fs
+++ day08/tests/Core.Tests/Tests.fs
@@ -16,3 +16,13 @@ let ``My bigg test`` () =
     bigSet |> scan |> compute |> should equal 1501
 
 
+
+[<Fact>]
+let ``My small test part 2`` () =
+    smallSet |> scan |> compute2 |> should equal 8
+
+[<Fact>]
+let ``My bigg test part 2`` () =
+    bigSet |> scan |> compute2 |> should equal 509
+
+
