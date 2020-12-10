f7b40a2db56253fb49ddca2782d9197589dcaff5
Allan Menoret <allan.menoret@gmail.com>
2020-12-10 13:27:08 +0100

    day 10 part 1 done

--- day10/src/Core/Library.fs
+++ day10/src/Core/Library.fs
@@ -1,5 +1,8 @@
 ﻿namespace Core
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module AdapterArray =
+    
+    
+    let compute (str:string) =
+        let (x,y,z) = ("0\n"+str).Split '\n' |> List.ofArray|> List.map int |> List.sort |> List.pairwise |> List.map (fun (x,y) -> y-x) |> List.fold (fun (x,y,z) n -> match n with |1->(x+1,y,z)|2->(x,y+1,z)|3->(x,y,z+1) |_ -> (x,y,z)) (0,0,0)
+        x*(z+1)
--- day10/tests/Core.Tests/Tests.fs
+++ day10/tests/Core.Tests/Tests.fs
@@ -1,8 +1,15 @@
 module Tests
 
-open System
 open Xunit
+open FsUnit
+
+open Core.AdapterArray
+open Inputs
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``small set part1`` () =
+    smallSet |> compute |> should equal 220
+
+[<Fact>]
+let ``big set part1`` () =
+    bigSet |> compute |> should equal 1920
