220b8c876c1bc5afeaa7206f05b717d1424ca994
Allan Menoret <allan.menoret@gmail.com>
2020-12-13 01:00:21 +0100

    day11 part 2 done

--- day11/src/Core/Library.fs
+++ day11/src/Core/Library.fs
@@ -6,6 +6,12 @@ module SeatingSystem =
     type Mapping = Map<(int*int), State>
     type SeatMap = {Mapping:Mapping; Limits:int*int}
 
+    let translate (_, v) = match v with | Occupied -> "#" | Floor -> "." | Empty -> "L"
+
+    let printMap (maps: SeatMap) =
+        printf "\n%i-%i {0,0}=%s\n" (maps.Limits |> fst) (maps.Limits |> snd) (translate ((0,0),maps.Mapping.[(0,0)]))
+        let t = maps.Mapping |> Map.toSeq |> Seq.sortBy (fun ((x,y),_) -> (y*100+x)) |> Seq.map translate
+        t |> Seq.chunkBySize ((maps.Limits |> snd) + 1) |> Seq.iter (Seq.reduce (+) >> printf "%s\n") 
 
     let excludedInvalidSeats (currentX,currentY) (limitX, limitY) value =
         match value with
@@ -35,28 +41,60 @@ module SeatingSystem =
         let res = seatMap.Mapping |> Map.toSeq |> Seq.map (computeNewState seatMap.Mapping seatMap.Limits)
         { seatMap with Mapping = Mapping res}
 
-    let translate (_, v) =
-        match v with 
-        | Occupied -> "#"
-        | Floor -> "."
-        | Empty -> "L"
 
     let countOccupiedSeats mapping =
         let res = mapping |> Map.toSeq |> Seq.filter (snd >> ((=) Occupied))
-        let t = mapping |> Map.toSeq |> Seq.sortBy (fun ((x,y),_) -> -(y*100+x)) |> Seq.map translate |> Seq.reduce (+)
-        printf "%s\n" t
         Seq.length res
 
-    let printMap (maps: SeatMap) =
-        printf "\n%i-%i {0,0}=%s\n" (maps.Limits |> fst) (maps.Limits |> snd) (translate ((0,0),maps.Mapping.[(0,0)]))
-        let t = maps.Mapping |> Map.toSeq |> Seq.sortBy (fun ((x,y),_) -> -(y*100-x)) |> Seq.map translate
-        t |> Seq.chunkBySize ((maps.Limits |> snd) + 1) |> Seq.iter (Seq.reduce (+) >> printf "%s\n") 
-
     let mapAreSame (map1: SeatMap) (map2: SeatMap)  =
-        map1.Mapping |> Map.toSeq |> Seq.exists (fun (pos, v) -> map2.Mapping.[pos] <> v) |> not
+        map1 = map2
+        // map1.Mapping |> Map.toSeq |> Seq.exists (fun (pos, v) -> map2.Mapping.[pos] <> v) |> not
 
     let compute (sitsMap: SeatMap) =
         let rec compute oldMap newMap =
             if mapAreSame oldMap newMap then countOccupiedSeats newMap.Mapping
             else compute newMap  (newCycle newMap)
         compute sitsMap (newCycle sitsMap)
+
+    let isOccupiedOnSight (limitX, limitY) value (mapping:Mapping) (func: int*int->int*int) =
+        let rec isOccupiedOnSight value =
+            match value with
+            | (x,_) when x < 0 || x > limitX -> 0
+            | (_,y) when y < 0 || y > limitY -> 0
+            | pos when mapping.[pos] = Empty -> 0
+            | pos when mapping.[pos] = Occupied -> 1
+            | _ -> (isOccupiedOnSight (func value))
+        isOccupiedOnSight (func value)
+
+    let straight (x,y) = (x+1,y)
+    let up (x,y) = (x,y-1)
+    let back (x,y) = (x-1,y)
+    let down (x,y) = (x,y+1)
+    let straightUp (x,y) = (x+1,y-1)
+    let straightDown (x,y) = (x+1,y+1)
+    let backUp (x,y) = (x-1,y-1)
+    let backDown (x,y) = (x-1,y+1)
+
+    let translationFuncs = [straight;up;back;down;straightUp;straightDown;backUp;backDown]
+
+    let countOccupiedAround2 pos (mapping:Mapping) limits =
+        translationFuncs |> List.sumBy (fun x -> isOccupiedOnSight limits pos mapping x)
+
+    let computeNewState2 mapping limit (pos,v) =
+        if v = Floor then (pos, v)
+        else
+            match (countOccupiedAround2 pos mapping limit, mapping.[pos]) with
+                | (0, _) -> ((pos), Occupied)
+                | (n, Occupied) when n >=5 -> ((pos), Empty)
+                | _ -> (pos, v)
+
+    let newCycle2 seatMap =
+        let res = seatMap.Mapping |> Map.toSeq |> Seq.map (computeNewState2 seatMap.Mapping seatMap.Limits)
+        { seatMap with Mapping = Mapping res}
+
+    let compute2 (sitsMap: SeatMap) =
+        let rec compute oldMap newMap =
+            if mapAreSame oldMap newMap then
+                countOccupiedSeats newMap.Mapping
+            else compute newMap  (newCycle2 newMap)
+        compute sitsMap (newCycle2 sitsMap)
--- day11/tests/Core.Tests/Tests.fs
+++ day11/tests/Core.Tests/Tests.fs
@@ -21,8 +21,21 @@ let ``smallset part 1 test computeNewState`` () =
     let mapping = smallSet2 |> scan
     computeNewState mapping.Mapping mapping.Limits ((0,0), Occupied) |> should equal ((0,0), Occupied)
 
-
 [<Fact>]
 let ``bigSet part 1`` () =
     bigSet |> scan |> compute |> should equal 2386
 
+
+
+[<Fact>]
+let ``smallset part 2`` () =
+    smallSet |> scan |> compute2 |> should equal 26
+
+[<Fact>]
+let ``smallset part 2 test computeNewState`` () =
+    let mapping = smallSet3 |> scan
+    computeNewState2 mapping.Mapping mapping.Limits ((6,0), Empty) |> should equal ((6,0), Occupied)
+
+[<Fact>]
+let ``bigSet part 2`` () =
+    bigSet |> scan |> compute2 |> should equal 2091
