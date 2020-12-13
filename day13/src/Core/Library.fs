997aad8764ee13199fa9f9998fab84ac854c2582
Allan Menoret <allan.menoret@gmail.com>
2020-12-13 17:07:39 +0100

    brute force day 13 part 2

--- day13/src/Core/Library.fs
+++ day13/src/Core/Library.fs
@@ -12,3 +12,24 @@ module ShuttleSearch =
         let {Timestamp=timestamp; BusID=busID} = parse str
         let (id, time) = busID |> List.map (fun x -> (x,(timestamp/x)*x+x)) |> List.minBy snd
         id*(time-timestamp)
+
+    
+    let parse2 (str:string) =
+        str.Split ',' |> List.ofArray |> List.mapi (fun i v -> i,v) |> List.filter (snd >> (<>) "x") |> List.map (fun (i,v) -> (bigint i), bigint (int v))
+
+    let allIdsWorked ids (target:bigint) (targetI:bigint) =
+        ids |> List.exists (fun (i,v) -> (target - targetI + i) % v <> 0I) |> not
+
+    let rec findTimestamp (i, targetId) ids c =
+        let target = targetId * c
+        printf "c: %A / target %A\n" c target
+        match allIdsWorked ids target i with
+        | true -> c
+        | false -> findTimestamp (i, targetId) ids (c+1I)
+
+    let search2 (str:string) =
+        let busIDs = parse2 str |> List.sortByDescending (fun (_,v)-> v)
+        let (i,v) = List.head busIDs
+        let c = findTimestamp (i,v) (List.skip 1 busIDs) 1I
+        let res = v*c-i
+        res
--- day13/tests/Core.Tests/Tests.fs
+++ day13/tests/Core.Tests/Tests.fs
@@ -16,3 +16,20 @@ let ``big set part 1`` () =
     bigSet |> search |> should equal 1835
 
 
+
+
+[<Theory>]
+[<InlineData("7,13,x,x,59,x,31,19", 1068781)>]
+[<InlineData("17,x,13,19", 3417)>]
+[<InlineData("67,7,59,61", 754018)>]
+[<InlineData("67,x,7,59,61", 779210)>]
+[<InlineData("67,7,x,59,61", 1261476)>]
+[<InlineData("1789,37,47,1889", 1202161486)>]
+let ``smallset part 2`` (input, (expectation:int)) =
+    input |> search2 |> should equal (bigint expectation)
+
+// [<Fact>]
+// let ``big set part 2`` () =
+//     bigSet2 |> search2 |> should equal 1835
+
+
