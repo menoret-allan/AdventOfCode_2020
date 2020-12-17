9244475014808c198ab0cab15a93b26f8c4d89d1
Allan Menoret <allan.menoret@gmail.com>
2020-12-17 23:44:53 +0100

    day 15 part 1

--- day15/src/Core/Library.fs
+++ day15/src/Core/Library.fs
@@ -1,5 +1,16 @@
 ﻿namespace Core
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module RambunctiousRecitation =
+    
+    
+    let compute (start: int list) limit =
+        let s = List.length start
+        let rec compute start next count =
+            if limit = count then start |> List.head
+            else
+                match List.tryFindIndex ((=)next) start with
+                | Some pos -> compute (next::start) (pos+1) (count+1)
+                | None -> compute (next::start) 0 (count+1)
+        compute (start |> List.rev |> List.skip 1) (List.last start) (s-1) 
+
+
--- day15/tests/Core.Tests/Tests.fs
+++ day15/tests/Core.Tests/Tests.fs
@@ -1,8 +1,40 @@
 module Tests
 
-open System
 open Xunit
+open FsUnit
+
+open Core.RambunctiousRecitation
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``part 1 test 1`` () =
+    compute [1;3;2] 2020 |> should equal 1
+[<Fact>]
+let ``part 1 test 2`` () =
+    compute [2;1;3] 2020 |> should equal 10
+[<Fact>]
+let ``part 1 test 3`` () =
+    compute [1;2;3] 2020 |> should equal 27
+[<Fact>]
+let ``part 1 test 4`` () =
+    compute [2;3;1] 2020 |> should equal 78
+[<Fact>]
+let ``part 1 test 5`` () =
+    compute [3;2;1] 2020 |> should equal 438
+[<Fact>]
+let ``part 1 test 6`` () =
+    compute [3;1;2] 2020 |> should equal 1836
+
+[<Fact>]
+let ``part 1 test 7`` () =
+    compute [0;3;6] 4 |> should equal 0
+
+[<Fact>]
+let ``part 1 test 8`` () =
+    compute [0;3;6] 5 |> should equal 3
+
+[<Fact>]
+let ``part 1 bigset`` () =
+    compute [14;3;1;0;9;5] 2020 |> should equal 614
+
+
+
