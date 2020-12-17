ff8f9677f9e9c383037a37f3599f62277de4582c
Allan Menoret <allan.menoret@gmail.com>
2020-12-18 00:40:31 +0100

    day 15 part 2

--- day15/src/Core/Library.fs
+++ day15/src/Core/Library.fs
@@ -2,7 +2,6 @@
 
 module RambunctiousRecitation =
     
-    
     let compute (start: int list) limit =
         let s = List.length start
         let rec compute start next count =
@@ -12,5 +11,29 @@ module RambunctiousRecitation =
                 | Some pos -> compute (next::start) (pos+1) (count+1)
                 | None -> compute (next::start) 0 (count+1)
         compute (start |> List.rev |> List.skip 1) (List.last start) (s-1) 
+
+    let compute2 (start: int list) limit =
+        let s = List.length start
+        let rec compute2 (m: Map<int, int>) next count l =
+            if limit = count then l
+            else
+                match m.TryFind(next) with
+                | Some pos -> compute2 (m.Add (next, count)) (count-pos) (count+1) next
+                | None -> compute2 (m.Add (next, count)) 0 (count+1) next
+        compute2 (Map(start |> List.take (s-1) |> List.mapi (fun i x -> (x, i)))) (List.last start) (s-1) 0
+    
+    let compute3 (start: int list) limit =
+        let s = List.length start
+        let a = Array.init (limit + List.max start) (fun _ -> -1)
+        start |> List.take (s-1) |> List.iteri (fun i x -> a.[x] <- i)
+        let rec compute3 next count l =
+            if limit = count then l
+            else
+                let x = a.[next]
+                a.[next] <- count
+                match x with
+                | -1 -> compute3 0 (count+1) next
+                | x -> compute3 (count-x) (count+1) next
+        compute3 (List.last start) (s-1) 0
 
 
--- day15/tests/Core.Tests/Tests.fs
+++ day15/tests/Core.Tests/Tests.fs
@@ -38,3 +38,90 @@ let ``part 1 bigset`` () =
 
 
 
+
+[<Fact>]
+let ``part 1 c2 test 1`` () =
+    compute2 [1;3;2] 2020 |> should equal 1
+[<Fact>]
+let ``part 1 c2 test 2`` () =
+    compute2 [2;1;3] 2020 |> should equal 10
+[<Fact>]
+let ``part 1 c2 test 3`` () =
+    compute2 [1;2;3] 2020 |> should equal 27
+[<Fact>]
+let ``part 1 c2 test 4`` () =
+    compute2 [2;3;1] 2020 |> should equal 78
+[<Fact>]
+let ``part 1 c2 test 5`` () =
+    compute2 [3;2;1] 2020 |> should equal 438
+[<Fact>]
+let ``part 1 c2 test 6`` () =
+    compute2 [3;1;2] 2020 |> should equal 1836
+
+
+
+[<Fact>]
+let ``part 1 c2 test 7`` () =
+    compute2 [0;3;6] 4 |> should equal 0
+
+[<Fact>]
+let ``part 1 c2 test 8`` () =
+    compute2 [0;3;6] 5 |> should equal 3
+
+
+
+
+[<Fact>]
+let ``part 2 test 1`` () =
+    compute3 [1;3;2] 2020 |> should equal 1
+[<Fact>]
+let ``part 2 test 2`` () =
+    compute3 [2;1;3] 2020 |> should equal 10
+[<Fact>]
+let ``part 2 test 3`` () =
+    compute3 [1;2;3] 2020 |> should equal 27
+[<Fact>]
+let ``part 2 test 4`` () =
+    compute3 [2;3;1] 2020 |> should equal 78
+[<Fact>]
+let ``part 2 test 5`` () =
+    compute3 [3;2;1] 2020 |> should equal 438
+[<Fact>]
+let ``part 2 test 6`` () =
+    compute3 [3;1;2] 2020 |> should equal 1836
+
+
+
+[<Fact>]
+let ``part 2 test 7`` () =
+    compute3 [0;3;6] 4 |> should equal 0
+
+[<Fact>]
+let ``part 2 test 8`` () =
+    compute3 [0;3;6] 5 |> should equal 3
+
+
+
+[<Fact>]
+let ``part 2 test 1 2`` () =
+    compute3 [1;3;2] 30000000 |> should equal 2578
+[<Fact>]
+let ``part 2 test 2 2`` () =
+    compute3 [2;1;3] 30000000 |> should equal 3544142
+[<Fact>]
+let ``part 2 test 3 2`` () =
+    compute3 [1;2;3] 30000000 |> should equal 261214
+[<Fact>]
+let ``part 2 test 4 2 `` () =
+    compute3 [2;3;1] 30000000 |> should equal 6895259
+[<Fact>]
+let ``part 2 test 5 2`` () =
+    compute3 [3;2;1] 30000000 |> should equal 18
+[<Fact>]
+let ``part 2 test 6 2`` () =
+    compute3 [3;1;2] 30000000 |> should equal 362
+
+
+[<Fact>]
+let ``part 2 bigset`` () =
+    compute3 [14;3;1;0;9;5] 30000000 |> should equal 1065
